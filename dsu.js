670c357e04d233ef6004164da7358ea0184d776a
martin <martin.papanek@gmail.com>
2014-07-07 00:31:32 +0100

    further fixes to dsu.js .. properly calling the drawTree in a callback

--- dsu.js
+++ dsu.js
@@ -35,28 +35,6 @@ function union (a, b, data, find_function) {
 }
 
 (function chart() {
-    var cbsFind = {};
-    var cbsUnion = {};
-    cbsFind[3] = function(a, data) {
-	console.log("in find with a=",a,"and root =",data[a].root);
-    }
-    cbsUnion[3] = function(r1,r2,a,b) {
-	console.log("for a=",a,"parent=",r1,"for b=",b,"parent=",r2);  
-    }
-    cbsUnion[8] = function(b) {
-	console.log("the new root is parent of ",b);  
-    }
-    cbsUnion[12] = function(a) {
-	console.log("the new root is parent of ",a);  
-    }
-    cbsUnion[16] = function(prob) {
-	console.log("both trees have the same size, we rolled a die and got", prob);  
-    }
-
-    var dsuFind = new Algorithm(find, cbsFind);
-    var dsuUnion = new Algorithm(union, cbsUnion);
-
-
     var margin = { left: 10, top: 30, right: 10, bottom: 50};
     var height = 150;
     var width = 1600;
@@ -68,7 +46,7 @@ function union (a, b, data, find_function) {
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-    var o = [{"name": 0}, {"name": 1}, {"name": 2}, {"name": 3}, {"name": 4}, {"name": 5}, {"name": 6}, {"name": 7}, {"name": 8}, {"name": 9}, {"name": 10}, {"name": 11}];
+    var o = [{"name": 0}, {"name": 1}, {"name": 2}, {"name": 3}, {"name": 4}, {"name": 5}, {"name": 6}]//, {"name": 7}, {"name": 8}, {"name": 9}, {"name": 10}, {"name": 11}];
 
     var data = []
     //linearize and add rank/root
@@ -149,10 +127,34 @@ function union (a, b, data, find_function) {
 
     }
 
-    // adding the shuffle function to array
-    Array.prototype.shuffle = function(){ 
-	for(var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x) ;
-    };
+
+
+    var cbsFind = {};
+    var cbsUnion = {};
+    cbsFind[3] = function(a, data) {
+	console.log("in find with a=",a,"and root =",data[a].root);
+    }
+    cbsUnion[3] = function(r1,r2,a,b) {
+	console.log("for a=",a,"parent=",r1,"for b=",b,"parent=",r2);  
+    }
+    cbsUnion[8] = function(b) {
+	console.log("the new root is parent of ",b);  
+    }
+    cbsUnion[12] = function(a) {
+	console.log("the new root is parent of ",a);  
+    }
+    cbsUnion[16] = function(prob) {
+	console.log("both trees have the same size, we rolled a die and got", prob);  
+    }
+    cbsUnion[28] = function(a,b,data) {
+	drawTreeFun(data[a],a);
+	drawTreeFun(data[b],b);
+    }
+
+    var dsuFind = new Algorithm(find, cbsFind);
+    var dsuUnion = new Algorithm(union, cbsUnion);
+
+
 
 
 
@@ -162,7 +164,7 @@ function union (a, b, data, find_function) {
         .attr("class", "prettyprint lang-js linenums:1")
 	.append("code")
         .attr("class", "language-js")
-        .text(dsuFind.decorated());
+        .text(dsuFind);
 
     d3.select("#dsu-tab .code")
 	.append("pre")
@@ -170,22 +172,12 @@ function union (a, b, data, find_function) {
         .attr("class", "prettyprint lang-js linenums:1")
 	.append("code")
         .attr("class", "language-js")
-        .text(dsuUnion.decorated());
+        .text(dsuUnion);
 
     /*calls google-prettify to make the code look nice*/
     prettyPrint();
 
-
-    var iter = 3;
-    while(iter > 0)
-    {
-	iter--;
-	//data.shuffle();
-	//union(data[0], data[1]);
-    }
-
     data.forEach(function(d) { d.rank = 0;});
-
     o.forEach(drawTreeFun);
 
 }());
