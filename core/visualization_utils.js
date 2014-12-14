72cbbb393740b38f965322ac22d9c49d51e124bc
dem42 <martin.papanek@gmail.com>
2014-12-14 18:19:35 -0500

    reorganizing core files into separate folder and adding visualization utils lib

---
+++ core/visualization_utils.js
@@ -0,0 +1,22 @@
+ALGORITHM_MODULE = (function(ALGORITHM_MODULE, d3, $) {
+    var _my = ALGORITHM_MODULE;
+    _my.visualization = {};
+    
+    /** animate moving growing a path */
+    _my.visualization.animatePath = function() {
+
+    };
+    
+    /** return a diagonal generator where the interpolation can be set
+      * so that various types of lines can be created */
+    _my.visualization.interpolatableDiagonal = function() {
+
+
+    };
+
+    /** add a svg:defs with an arrow market to be used with non-growing paths */
+    _my.visualization.appendMarkerDefs = function() {
+
+    };
+
+}(ALGORITHM_MODULE || {}, d3, $);
