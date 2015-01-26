7d21ae277a46d08d1006d4d2d0be3d6051cc639b
dem42 <martin.papanek@gmail.com>
2015-01-26 11:30:47 -0500

    fixing up raptoring and pretty print

--- core/main.js
+++ core/main.js
@@ -12,11 +12,23 @@ $(function (ALGORITHM_MODULE) {
 	cache: false
     });
 
+
+    // using ready for now because load doesn't get triggered 
+    // when our internet drops and the twitter/git imgs don't get loaded
+    var prettification_complete = false;
+    $("body").ready(function() {
+	prettyPrint();
+	prettification_complete = true;
+	var current_tab = window.location.hash;
+	if (current_tab !== undefined && current_tab !== null && current_tab !== "") {
+	    raptoringItUp(current_tab);
+	}
+    });
     
     $('a[data-toggle="tab"]').off('shown.bs.tab');
     $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
 	resizingSvg(e)
-	raptoringItUp(e);
+	raptorFromEvent(e);
     });
     $('a', "#algoTabs").off("click");
     $('a', "#algoTabs").on('click', function(e) {
@@ -55,84 +67,20 @@ $(function (ALGORITHM_MODULE) {
     }( jQuery ));
     $("#algoTabs").stickyTabs(); // activating sticky tabs
     
-    /*
 
-    var loadedVisualizations = {};
-    var newAlgoAdded = false;
-    // this callback fires whenever an ajax event completes
-    $(document).ajaxComplete(function() { 
-	if (newAlgoAdded) {
-	    prettyPrint.call({});
-
-	    console.log("registering callback", newAlgoAdded);
-	    //first deregister old event listeners so that we don't have the same fnc attached many times
-	    $('a[data-toggle="tab"]').off('shown.bs.tab');
-	    // attach a callback on shown tab to adjust svg sizes dynamically
-	    // this is needed to make sure our visualizations fit onto smaller screens
-	    $('a[data-toggle="tab"]').on('shown.bs.tab', resizingSvg);
-	    // again we need to turn off previous click handlers so that we don't end up with too many
-	    $('a', "#algoTabs").off("click");
-	    $('a', "#algoTabs").on('click', function(e) {
-		// modify the history and the url
-		// although the firefox back button does look quite strange after this
-		//history.pushState(null, null, this.href);
-		// alternative solution
-		window.location.href = $(this).attr("href"); 
-		window.scrollBy(0, -window.pageYOffset);
-	    });
-	}
-    });
-    */
-
-    /*
-    function updateVisualizations(data) {
-        var N = data.length;
-	newAlgoAdded = false;
-        for (var i = 0; i < N; ++i) {
-	    if (!loadedVisualizations.hasOwnProperty(data[i])) {
-		loadedVisualizations[data[i]] = data[i];
-		newAlgoAdded = true;
-		console.log("new", data[i]);
-		// these get script functions will be processed one at a time by the browser because they will be tasks
-		// on a work queue and will be processed when the browser is ready
-		// that means we don't have to worry about race conditions between multiple visualizations scripts 
-		$('head').append('<link rel="stylesheet" href="visualizations/' + data[i] + '.css" type="text/css" />');
-
-		(function(algo_name) { 
-		    var url = "visualizations/" + algo_name + ".js";
-		    $.getScript(url)
-			.done(function( data, textStatus, jqxhr ) {
-			    console.debug( "Download of algo :" + algo_name, textStatus ); // Success
-			})
-			.fail(function(jqxhr, settings, exception) {
-			    bootbox.alert("Problem loading algorithm: \"" + algo_name + "\".<br \><br \>" + exception);
-			});
-		}(data[i]));
-	    }
-        }
-    }
-
-    $.ajax("visualizations").done(function(data) {
-	updateVisualizations(data);
-        $("#algoTabs a:first").tab("show");
-        console.debug(data);
-	updater();
-    });
-    
-    function updater() {
-        setTimeout(function() {
-            $.ajax("visualizations").done(function(data) {
-		updateVisualizations(data);
-            });
-	    updater();
-        }, 5000);
-    }
-    */
-
-    function raptoringItUp(e) {
+    function raptorFromEvent(e) {
 	var tabz =  $(e.target).attr("data-tab-id");
-	var tab = $(tabz);
-	if (tab.attr("data-raptored") == true) {
+	raptoringItUp(tabz);
+    }
+
+    function raptoringItUp(tab_id) {
+	console.debug("raptoring", tab_id);
+	if (!prettification_complete) {
+	    return;
+	}
+	var tab = $(tab_id);
+	if (tab.attr("data-raptored") == "true") {
+	    console.debug("already raptored", tab_id);
 	    return;
 	}
 	var callbacks = _my.AlgorithmUtils.getPrettyPrintCallbacks();
