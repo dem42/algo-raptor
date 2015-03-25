7f66f6fa4a3dd1c4ab34c885f2e73e02aeafeb6a
dem42 <martin.papanek@gmail.com>
2015-03-25 01:26:08 +0000

    adding bower to algoraptor

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,15 +45,40 @@ module.exports = function(grunt) {
 	},
 	jshint: {
 	    all: ['Gruntfile.js', 'core/*.js', 'visualizations/*.js']
+	},
+	bower: {
+	    install: {}
+	},
+	copy: {
+	    options: {
+		separator: '',
+		punctuation: '',
+	    },
+	    bower_files: {
+		files: [
+		    {dest:'lib/d3/d3.min.js', src:'bower_components/d3/d3.min.js'},
+		    {dest:'lib/jquery/jquery.min.js', src:'bower_components/jquery/jquery.min.js'},
+		    {dest:'lib/bootstrap/bootstrap.min.css', src:'bower_components/bootstrap/dist/css/bootstrap.min.css'},
+		    {dest:'lib/bootstrap/bootstrap.min.js', src:'bower_components/bootstrap/dist/js/bootstrap.min.js'},
+		    {dest:'lib/fonts/glyphicons-halflings-regular.eot', src:'lib/bootstrap/glyphicons-halflings-regular.eot'},
+		    {dest:'lib/fonts/glyphicons-halflings-regular.ttf', src:'lib/bootstrap/glyphicons-halflings-regular.ttf'},
+		    {dest:'lib/fonts/glyphicons-halflings-regular.woff2', src:'lib/bootstrap/glyphicons-halflings-regular.woff2'},
+		    {dest:'lib/fonts/glyphicons-halflings-regular.svg', src:'lib/bootstrap/glyphicons-halflings-regular.svg'},
+		    {dest:'lib/fonts/glyphicons-halflings-regular.woff', src:'lib/bootstrap/glyphicons-halflings-regular.woff'}		]
+	    }
 	}
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-express');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-bower-task');
 
-    grunt.registerTask('default', ['concat', 'qunit', 'express', 'watch']);
+    grunt.registerTask('default', ['bower', 'copy', 'concat', 'qunit', 'express', 'watch']);
+
+    grunt.registerTask('no-test', ['bower', 'copy', 'concat', 'express', 'watch']);
 };
